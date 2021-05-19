e3a0658788f00610c0f6be4925011e671ad84bf6
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-05-19 22:57:52 +0300

    Выполнено задание 3

--- index.js
+++ index.js
@@ -40,3 +40,41 @@ const logger2 = updateUsers => console.table(updateUsers);
 
 toggleUserState(users, 'Mango').then(logger2);
 toggleUserState(users, 'Lux').then(logger2);
+
+//Задание 3
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  const delay = randomIntegerFromInterval(200, 500);
+  return new Promise((resolve, reject) => {
+    let id = transaction.id;
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+      if (canProcess) {
+        const result = { id, delay };
+        resolve(result);
+      } else {
+        reject(Error(id));
+      }
+    }, delay);
+  });
+};
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
+};
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+/*
+ * Должно работать так
+ */
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
