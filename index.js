7bf98e522e8abe6f10d6d0673c610f02f92d506a
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-05-19 22:20:13 +0300

    Выполнено задание 2

--- index.js
+++ index.js
@@ -14,3 +14,29 @@ const logger = time => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+//Задание 2
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updateUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updateUsers);
+  });
+};
+
+const logger2 = updateUsers => console.table(updateUsers);
+/*
+ * Должно работать так
+ */
+
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
