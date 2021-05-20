ee894d41a01a2f0e0b6fda7d4fdbc4e163e1d9c7
Dmitriy Dudnik <ddudnik79@gmail.com>
2021-05-20 22:39:10 +0300

    исправлена ошибка в  3 задании

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ toggleUserState(users, 'Mango').then(logger2);
 toggleUserState(users, 'Lux').then(logger2);
 
 //Задание 3
+//Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -61,8 +63,8 @@ const makeTransaction = transaction => {
     }, delay);
   });
 };
-const logSuccess = ({ id, time }) => {
-  console.log(`Transaction ${id} processed in ${time} ms`);
+const logSuccess = ({ id, delay }) => {
+  console.log(`Transaction ${id} processed in ${delay} ms`);
 };
 const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
